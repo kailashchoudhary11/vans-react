54bd8d81948f3ff4539be1828acad16a18002508
kai <kailashjat604@gmail.com>
2023-04-21 18:51:38 +0530

    Basic style

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,11 +1,45 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import "../server/server";
+import "./Vans.css";
 
 export default function Vans() {
+    const [vansData, setVansData] = React.useState([]);
+
+    React.useEffect(() => {
+        fetch("/api/vans")
+        .then(res => res.json())
+        .then(data => setVansData(data.vans));
+    }, []); 
+    console.log(vansData);
+    const vansComps = vansData.map((van, i) => 
+            <div className="van" key={i}>
+                <img className="van-pic" src={van.imageUrl} />
+                <div className="info">
+                    <span className="name">
+                        {van.name}
+                    </span>
+                    <span className="price">
+                        ${van.price}/day
+                    </span>
+                </div>
+                <button className="type">
+                    {van.type.charAt(0).toUpperCase() + van.type.slice(1)}
+                </button>
+            </div>
+        );
+
     return (
         <>
             <Navbar />
-            <h1>Vans page goes here!</h1>
+            {
+                vansData.length > 0 ? 
+                <div className="vans">
+                    {vansComps}
+                </div>
+                :
+                <h1>Loading...</h1>
+            }
         </>
     );
 }
