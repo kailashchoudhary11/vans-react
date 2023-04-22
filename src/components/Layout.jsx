9703e2b74fe2ea08d402a115ac3011d2850cc066
kai <kailashjat604@gmail.com>
2023-04-22 18:04:52 +0530

    Add Footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import Footer from "./Footer";
+import "./Layout.css";
 
 export default function Layout() {
     return (
-        <>
+        <div className="site-wrapper">
             <Header />
-            <Outlet />
-        </>
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
     );
 }
