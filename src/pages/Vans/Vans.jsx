ad7f5269c822acd1e2d35b90e88beafbc0b970fd
kai <kailashjat604@gmail.com>
2023-04-25 22:43:37 +0530

    Add filter vans feature

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -33,9 +33,28 @@ export default function Vans() {
         </div>
     );
 
+    function handleFilterChange(key, value) {
+        setSearchParams(prevParams => {
+            if (value == null) {
+                prevParams.delete(key);
+            } else {
+                prevParams.set(key, value);
+            }
+            return prevParams;
+        });
+    }
     return (
         <div className="van-list-container">
             <h1>Explore our van options</h1>
+            <div className="van-list-filter-buttons">
+                <button className={`van-type simple ${typeFilter == "simple" ? "selected" : ""}`} onClick={() => handleFilterChange("type", "simple")}>Simple</button>
+                <button className={`van-type rugged ${typeFilter == "rugged" ? "selected" : ""}`} onClick={() => handleFilterChange("type", "rugged")}>Rugged</button>
+                <button className={`van-type luxury ${typeFilter == "luxury" ? "selected" : ""}`} onClick={() => handleFilterChange("type", "luxury")}>Luxury</button>
+                {
+                    typeFilter  && 
+                    <button className="van-type clear-filters" onClick={() => handleFilterChange("type", null)}>Clear</button>
+                }
+            </div>
             <div className="van-list">
                 {vansComps}
             </div>
